e1e46c1c20b2ea6fc7cd3c169a8c5594f4fd94ab
kingdarkne <kingdarkne@users.noreply.github.com>
2026-05-24 21:27:16 -0400

    Fix broken fenix-police submodule breaking Actions checkout.

---
+++ resources/fenix-police/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Fenix'
+description 'Police'
+version '1.0'
+lua54 'yes'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
